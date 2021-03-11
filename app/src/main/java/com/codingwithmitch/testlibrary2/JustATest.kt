54dd74fc26e0308bae2e1dc8ecef3faa6083446d
mitch <mitch@tabian.ca>
2021-03-11 15:24:16 -0800

    first

---
+++ app/src/main/java/com/codingwithmitch/testlibrary2/JustATest.kt
@@ -0,0 +1,8 @@
+package com.codingwithmitch.testlibrary2
+
+class JustATest {
+
+    fun getTestObject(): Mitch {
+        return Mitch()
+    }
+}
---
+++ app/src/main/java/com/codingwithmitch/testlibrary2/Mitch.kt
@@ -0,0 +1,8 @@
+package com.codingwithmitch.testlibrary2
+
+class Mitch {
+
+    fun info(): String {
+        return "Hey it's Mitch. V2"
+    }
+}
